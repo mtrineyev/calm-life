a4dfd04ff157d537fcaadc67238ecf4d49b19170
Maksym Trineyev <mtrineyev@gmail.com>
2021-08-27 12:34:45 +0300

    first commit

---
+++ main.py
@@ -0,0 +1,104 @@
+"""
+Stop the process of hanging scraping jobs
+
+For detailed setup, deployment and run instructions see readme.md file
+
+© 2021 MediaMonitoringBot, written by Maksym Trineiev
+"""
+
+from configparser import ConfigParser
+import logging
+from os import system
+import pickle
+import requests
+
+config = ConfigParser()
+config.read('config.ini')
+logging.basicConfig(
+    filename=config['Logging']['FILE_NAME'],
+    format='%(asctime)s [%(name)s] %(levelname)s: %(message)s',
+    datefmt='%Y-%m-%d %H:%M:%S',
+    level=int(config['Logging']['LEVEL']))
+
+
+class ActiveProcesses:
+    """
+    Get list of the currently running scrapy jobs (aka 'processes')
+    and terminate old hanging ones
+    """
+    proc_file = 'processes.txt'
+    pickle_file = 'processes.pickle'
+    processes_list = f"{config['Main']['PROCESSES_LIST']} > {proc_file}"
+    kill_command = config['Main']['KILL_COMMAND']
+    process_name = config['Main']['PROCESS_NAME'].lower()
+    seen_times = config['Main'].getint('SEEN_TIMES')
+    heath_check_url = config['Misc']['HEALTH_CHECK_URL']
+    slack_webhook = config['Misc']['SLACK_MEDIAMONITORINGBOT_WEBHOOK']
+
+    def __init__(self) -> None:
+        if self.heath_check_url:
+            requests.get(f'{self.heath_check_url}/start', timeout=5)
+        try:
+            with open(self.pickle_file, 'rb') as f:
+                self.seen_processes = pickle.load(f)
+            logging.info(f'Read {len(self.seen_processes)} seen processes.')
+        except FileNotFoundError:
+            logging.warning(f'File "{self.pickle_file}" not found. Used empty one.')
+            self.seen_processes = dict()
+        return
+
+    def get_current_processes(self) -> int:
+        """
+        Call OS command of list processes, parse python processes
+        and count process seen times
+        """
+        current_processes = dict()
+        returned_value = system(self.processes_list)
+        if returned_value:
+            logging.warning(f'Processes list terminated with error {returned_value}')
+            return returned_value
+        with open(self.proc_file, 'r') as f:
+            processes = f.readlines()
+        for process in processes:
+            parsed = process.strip().lower().split(' ')
+            if parsed[-1] == self.process_name:
+                process_number = parsed[0]
+                current_processes[process_number] = self.seen_processes.get(process_number, 0) + 1
+        self.seen_processes = current_processes
+        return 0
+
+    def terminate_hang_processes(self) -> None:
+        """
+        Find processes seen more then self.seen_times
+        and kill them
+        """
+        for key, value in self.seen_processes.items():
+            if value >= self.seen_times:
+                returned_value = system(f'{self.kill_command} {key}')
+                if returned_value:
+                    logging.error(f'Can\'t kill PID {key}')
+                else:
+                    self.seen_processes[key] = 0
+        self.seen_processes = {k: v for k, v in self.seen_processes.items() if v}
+        return
+
+    def save_seen_processes(self) -> None:
+        """
+        Save count of seen processes
+        @return: None
+        """
+        logging.info(f'Saving {len(self.seen_processes)} current processes.')
+        with open(self.pickle_file, 'wb') as f:
+            pickle.dump(self.seen_processes, f)
+        if self.heath_check_url:
+            requests.get(self.heath_check_url)
+        return
+
+
+if __name__ == '__main__':
+    p = ActiveProcesses()
+    p.get_current_processes()
+    p.terminate_hang_processes()
+    p.save_seen_processes()
+else:
+    print(__doc__)
---
+++ readme.md
@@ -0,0 +1,36 @@
+# Stop the process of hanging scraping jobs
+
+
+## Description
+The Python script for killing the hanged scrapy processes.
+
+
+## Setup and deployment
+- `git clone https://github.com/mtrineyev/calm-life.git`
+- `cd calm-life`
+- `cp config.ini.example config.ini`
+- `nano config.ini` and set the variables as described in the comments
+- `sudo apt-get install python3-dev`
+- `sudo apt-get install python3-venv`
+- `python3 -m venv venv`
+- `source venv/bin/activate`
+- `pip install -r requirements.txt`
+- `nano calm-life.sh` and copy the code below
+```
+        #!/bin/bash
+        cd /home/USER/calm-life
+        source venv/bin/activate
+        python main.py
+        deactivate
+```
+- `chmod +x calm-life.sh`
+
+
+## To test the script
+`cd /home/USER/calm-life`, run `calm-life.sh` and check the file `processes.txt`
+
+
+## The licence
+The script is free software written by Maksym Trineyev (mtrineyev@gmail.com).
+
+It comes with ABSOLUTELY NO WARRANTY, to the extent permitted by applicable law.
